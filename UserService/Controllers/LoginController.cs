8d5f536161e07443887cc03a27673b2028367949
Okular28 <pawel.siola2003@gmail.com>
2025-06-18 20:27:42 +0200

    wip JWT

---
+++ UserService/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Models.LoginRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username) || string.IsNullOrWhiteSpace(request.Password))
+            {
+                return BadRequest("Username and password are required.");
+            }
+
+            return Ok($"Dane odebrane: {request.Username} / {request.Password}");
+        }
+    }
+}
