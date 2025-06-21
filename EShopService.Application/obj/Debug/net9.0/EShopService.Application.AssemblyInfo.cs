4baa70ab674a686b0517afe330e162cfca3fbbbc
BrunonSocha <brunon.socha@gmail.com>
2025-06-21 15:20:32 +0000

    fix

--- EShopService.Application/obj/Debug/net9.0/EShopService.Application.AssemblyInfo.cs
+++ EShopService.Application/obj/Debug/net9.0/EShopService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EShopService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ad4d8ed3ea8af016d2bc238b12111055bdaa6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7763bcda054235eccb615c131c28d03c75fcc0a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EShopService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EShopService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EShopService.Domain/obj/Debug/net9.0/EShopService.Domain.AssemblyInfo.cs
+++ EShopService.Domain/obj/Debug/net9.0/EShopService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EShopService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ad4d8ed3ea8af016d2bc238b12111055bdaa6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7763bcda054235eccb615c131c28d03c75fcc0a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EShopService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EShopService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
