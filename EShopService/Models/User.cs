1f1e89369fda45a35091b470df028112ddb88a03
BrunonSocha <brunon.socha@gmail.com>
2025-06-21 12:46:04 +0000

    previous merge changed userid back to an int, fixed

--- EShopService/Models/User.cs
+++ EShopService/Models/User.cs
@@ -2,7 +2,7 @@ namespace EShopService;
 
 public class User
 {
-    public int UserId { get; set; }
+    public Guid UserId { get; set; }
     public string Username { get; set; } = default!;
 
     public string Email { get; set; } = default!;
